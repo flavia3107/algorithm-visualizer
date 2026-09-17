b57e4ad70235b0febb8179deeb93bc432ffebdb8
Flavia Kosti <flaviakosti@gmail.com>
2026-09-17 18:55:15 -0400

    Algorithm-Visualizer: Main Layout - P6

--- src/app/components/sidenav/sidenav.ts
+++ src/app/components/sidenav/sidenav.ts
@@ -1,9 +1,13 @@
+import { KeyValuePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ALGORITHM_TREE } from '../../../configs/algorithm-tree';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [KeyValuePipe],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss',
 })
-export class Sidenav {}
+export class Sidenav {
+  readonly algorithmConfig = ALGORITHM_TREE;
+}
