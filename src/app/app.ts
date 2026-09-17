dc64ee89640a315907295b0566b7906e5c4fe2c6
Flavia Kosti <flaviakosti@gmail.com>
2026-09-17 18:44:16 -0400

    Algorithm-Visualizer: Main Layout - P5

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { Controls } from './components/controls/controls';
+import { DataStructure } from './components/data-structure/data-structure';
+import { Details } from './components/details/details';
+import { ExecutionLog } from './components/execution-log/execution-log';
+import { Sidenav } from './components/sidenav/sidenav';
+import { Visualizer } from './components/visualizer/visualizer';
 
 @Component({
   selector: 'app-root',
+  imports: [Controls, DataStructure, Details, ExecutionLog, Sidenav, Visualizer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
