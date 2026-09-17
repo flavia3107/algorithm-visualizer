952605236e70037d6a53481a223843d0890a58e4
Flavia Kosti <flaviakosti@gmail.com>
2026-09-17 18:05:17 -0400

    Algorithm-Visualizer: Main Layout - P1

---
+++ src/app/configs/algorithm-tree.ts
@@ -0,0 +1,48 @@
+export const ALGORITHM_TREE = {
+	sorting: [
+		{
+			key: 'bubble_sort',
+			title: 'Bubble Sort',
+			icon: ''
+		},
+		{
+			key: 'selection_sort',
+			title: 'Selection Sort',
+			icon: ''
+		},
+		{
+			key: 'merge_sort',
+			title: 'Merge Sort',
+			icon: ''
+		}
+	],
+	searching: [
+		{
+			key: 'binnary_search',
+			title: 'Binnary Search',
+			icon: ''
+		},
+		{
+			key: 'linear_search',
+			title: 'Linear Search',
+			icon: ''
+		}
+	],
+	graphs: [
+		{
+			key: 'dijkstra',
+			title: 'Dijkstra\'s Algorithm',
+			icon: ''
+		},
+		{
+			key: 'bfs',
+			title: 'Breadth-First Search',
+			icon: ''
+		},
+		{
+			key: 'a*',
+			title: 'A*',
+			icon: ''
+		}
+	]
+};
