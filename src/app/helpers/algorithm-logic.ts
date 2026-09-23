6e75771913448e4b78084d1b75a06560ece533d3
Flavia Kosti <flaviakosti@gmail.com>
2026-09-23 17:13:38 -0400

    Algorithm-Visualizer: Visualizer - P13

--- src/app/helpers/algorithm-logic.ts
+++ src/app/helpers/algorithm-logic.ts
@@ -3,11 +3,12 @@
 import { VisualizationStep } from "../../configs/algorithm-config";
 import { GridNode, generateDijkstraSteps, generateBFSSteps, generateDFSSteps, generateAStarSteps } from "./graph-algorithms";
 import { generateBinarySearchSteps, generateLinearSearchSteps } from "./searching-algorithms";
-import { generateBubbleSortSteps, generateSelectionSortSteps } from "./sorting-algorithms";
+import { generateBubbleSortSteps, generateMergeSortSteps, generateSelectionSortSteps } from "./sorting-algorithms";
 
 export const ALGORITHM_MAP: Record<string, (data: any) => VisualizationStep[]> = {
 	bubble_sort: (data: number[]) => generateBubbleSortSteps(data),
 	selection_sort: (data: number[]) => generateSelectionSortSteps(data),
+	merge_sort: (data: number[]) => generateMergeSortSteps(data),
 
 	binary_search: (data: { list: number[]; target: number }) => generateBinarySearchSteps(data),
 	linear_search: (data: { list: number[]; target: number }) => generateLinearSearchSteps(data),
